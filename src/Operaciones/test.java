2ff50350c803cd11a2163186cdda43b4de43cb61
AlvarocBernal <alvarocbernal1995@gmail.com>
2016-03-11 11:55:06 +0100

    Update test.java

--- src/Operaciones/test.java
+++ src/Operaciones/test.java
@@ -5,6 +5,7 @@ public class test {
 
     public static void main(String[] args) {
            suma(4,5);
+           suma(3,7);
     }
     public static void suma(int a, int b){
         int suma=a+b;
